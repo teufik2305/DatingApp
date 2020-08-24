c55431fc2125f30ca0c9a461af28d96cd0c5182a
Teufik2305 <teufik97@gmail.com>
2020-08-24 13:49:55 +0200

    End of Section 4

--- DatingApp-SPA/src/app/register/register.component.spec.ts
+++ DatingApp-SPA/src/app/register/register.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { WeatherforecastComponent } from './weatherforecast.component';
+import { RegisterComponent } from './register.component';
 
-describe('WeatherforecastComponent', () => {
-  let component: WeatherforecastComponent;
-  let fixture: ComponentFixture<WeatherforecastComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WeatherforecastComponent ]
+      declarations: [ RegisterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherforecastComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
